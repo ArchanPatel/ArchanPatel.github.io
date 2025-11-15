68d75be4e14cc1f71ddf7fc6028aaa5d8c021206
Archan Patel <55643686+ArchanPatel@users.noreply.github.com>
2025-11-14 23:26:55 -0500

    Add TODO comment for App component test
    Added a note to implement a test for the App component.

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-//TODO
+//TODO add test
 test('renders learn react link', () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
